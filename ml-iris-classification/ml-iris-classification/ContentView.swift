2884ce9190c98202224506a6241c73fd9e61bfe5
Nick Everitt <nicholaseveritt@gmail.com>
2023-03-03 11:30:20 +0000

    Create xcode project

---
+++ ml-iris-classification/ml-iris-classification/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ml-iris-classification
+//
+//  Created by Nick on 03/03/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ml-iris-classification/ml-iris-classification/ml_iris_classificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ml_iris_classificationApp.swift
+//  ml-iris-classification
+//
+//  Created by Nick on 03/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct ml_iris_classificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
