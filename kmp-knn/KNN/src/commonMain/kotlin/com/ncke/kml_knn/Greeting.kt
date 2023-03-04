804462767217d7cae5fea68c519f08ef7fba1f18
Nick Everitt <nicholaseveritt@gmail.com>
2023-03-04 07:23:53 +0000

    Remove unused kotlin boilerplate

--- kmp-knn/KNN/src/commonMain/kotlin/com/ncke/kml_knn/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.ncke.kml_knn
-
-class Greeting {
-    private val platform: Platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- kmp-knn/KNN/src/commonMain/kotlin/com/ncke/kml_knn/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.ncke.kml_knn
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
